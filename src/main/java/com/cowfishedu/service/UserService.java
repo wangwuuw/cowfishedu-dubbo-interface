f9974f6f410af1607763d797731434dd21a0839e
wangwu <wangwu123mtr@sina.com>
2018-01-29 22:32:23 +0800

    common-jar

---
+++ src/main/java/com/cowfishedu/service/UserService.java
@@ -0,0 +1,14 @@
+package com.cowfishedu.service;
+
+/**
+ * @classDesc： 功能描述：（）
+ * @author：王武
+ * @createTime 2018/1/29
+ * @verson: v1.0
+ * @copyright: 上海江豚教育科技有限公司
+ */
+public interface UserService {
+  //提供服务,通过用户id查找信息
+    public String getUserId(Integer id);
+
+}
